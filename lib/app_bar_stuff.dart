8d0feb548cb192ba132db87adff8750eb59e0535
Hans van Dam <hans@damsoft.nl>
2023-09-04 20:19:20 +0200

    Quick and Dirty Langchain insertion

--- lib/app_bar_stuff.dart
+++ lib/app_bar_stuff.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:langchain_openai/langchain_openai.dart';
+import 'package:langchain/langchain.dart';
 
 PreferredSizeWidget createAppBar(
     Function() showBottomSheet
@@ -53,6 +54,19 @@ class _LangFieldState extends State<LangField> {
     // maxLength: 10,
     // maxLengthEnforcement: MaxLengthEnforcement.none,
     controller: _controllerOutlined,
+    onSubmitted: (final String value) {
+      var apiKey2 = "";
+      var client = OpenAIClient.instanceFor(apiKey: apiKey2);
+      final llm = ChatOpenAI(apiClient: client);
+      sendToOpenai(llm, this._controllerOutlined.text);
+
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   SnackBar(
+      //     content: Text('You typed: $value'),
+      //     duration: const Duration(seconds: 1),
+      //   ),
+      // );
+    },
     decoration: InputDecoration(
       prefixIcon: const Icon(Icons.search),
       suffixIcon: _ClearButton(controller: _controllerOutlined),
@@ -63,6 +77,13 @@ class _LangFieldState extends State<LangField> {
       // errorText: 'error text',
     ),
   );
+
+  Future<void> sendToOpenai(ChatOpenAI llm, String query) async {
+    final result = await llm([ChatMessage.human(query)]);
+    setState(() {
+      _controllerOutlined.text = result.content.trim();
+    });
+  }
 }
 
 
