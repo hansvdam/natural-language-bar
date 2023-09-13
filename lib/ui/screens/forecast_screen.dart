029c5bfe39bc8a931bbc7e09fcec3dcc31ed3db3
Hans van Dam <hans@damsoft.nl>
2023-09-13 10:21:01 +0200

    update to forecast screen

--- lib/ui/screens/forecast_screen.dart
+++ lib/ui/screens/forecast_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../utils.dart';
 
@@ -6,11 +9,10 @@ const smallSpacing = 10.0;
 
 class ForecastScreen extends StatefulWidget {
   /// Creates a RootScreen
-  ForecastScreen(
-      {required this.label,
-      required this.detailsPath,
-      required this.bottomSheetFunction,
-      Key? key})
+  ForecastScreen({required this.label,
+    required this.detailsPath,
+    required this.bottomSheetFunction,
+    Key? key})
       : super(key: key);
 
   /// The label
@@ -25,6 +27,41 @@ class ForecastScreen extends StatefulWidget {
   State<ForecastScreen> createState() => _ForecastScreenState();
 }
 
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
 class _ClearButton extends StatelessWidget {
   const _ClearButton({required this.controller});
 
@@ -32,9 +69,9 @@ class _ClearButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => IconButton(
-    icon: const Icon(Icons.clear),
-    onPressed: () => controller.clear(),
-  );
+        icon: const Icon(Icons.clear),
+        onPressed: () => controller.clear(),
+      );
 }
 
 class _ForecastScreenState extends State<ForecastScreen> {
@@ -42,37 +79,55 @@ class _ForecastScreenState extends State<ForecastScreen> {
 
   _ForecastScreenState();
 
+  Future<Album>? futureAlbum;
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> children = [];
+    var children2 = <Widget>[
+      Text('Screen ${widget.label}',
+          style: Theme.of(context).textTheme.titleLarge),
+      const Padding(padding: EdgeInsets.all(4)),
+      Padding(
+        padding: const EdgeInsets.all(smallSpacing),
+        child: TextField(
+          controller: _controllerOutlined,
+          onSubmitted: (final String value) {
+            setState(() {
+              futureAlbum = fetchAlbum();
+            });
+          },
+          decoration: InputDecoration(
+            prefixIcon: const Icon(Icons.search),
+            suffixIcon: _ClearButton(controller: _controllerOutlined),
+            labelText: 'Area on earth',
+            hintText: 'Area on earth',
+            border: const OutlineInputBorder(),
+          ),
+        ),
+      ),
+      if (futureAlbum != null)
+        FutureBuilder<Album>(
+          future: futureAlbum,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.title);
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            return const CircularProgressIndicator();
+          },
+        ),
+    ];
+    children.addAll(children2);
     return Scaffold(
       appBar: createAppBar(() {
         widget.bottomSheetFunction(context);
-        // bottomsheet(context);
       }),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text('Screen ${widget.label}',
-                style: Theme.of(context).textTheme.titleLarge),
-            const Padding(padding: EdgeInsets.all(4)),
-            Padding(
-              padding: const EdgeInsets.all(smallSpacing),
-              child: TextField(
-                controller: _controllerOutlined,
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.search),
-                  suffixIcon: _ClearButton(controller: _controllerOutlined),
-                  labelText: 'Outlined',
-                  hintText: 'hint text',
-                  helperText: 'supporting text',
-                  border: const OutlineInputBorder(),
-                ),
-              ),
-            ),          ],
-        ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: children,
       ),
     );
   }
 }
-
