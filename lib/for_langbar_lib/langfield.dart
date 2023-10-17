03db6f6cab9f0e4104d422b242a7eb5773a23286
Hans van Dam <hans@damsoft.nl>
2023-10-17 14:35:16 +0200

    move openai sending outside langfield file

--- lib/for_langbar_lib/langfield.dart
+++ lib/for_langbar_lib/langfield.dart
@@ -1,17 +1,9 @@
-import 'package:dart_openai/dart_openai.dart' as dart_openai;
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
-import 'package:langbar/for_langbar_lib/generic_screen_tool.dart';
+import 'package:langbar/for_langbar_lib/send_to_llm.dart';
 import 'package:langbar/for_langbar_lib/speech.dart';
-import 'package:langchain/langchain.dart';
-import 'package:langchain_openai/langchain_openai.dart';
 import 'package:provider/provider.dart';
 
-import '../openAIKey.dart';
-import '../routes.dart';
 import 'langbar_states.dart';
-import 'llm_go_route.dart';
 
 class LangField extends StatefulWidget {
   final bool showHistoryButton;
@@ -43,7 +35,7 @@ class _LangFieldState extends State<LangField> {
           textAlignVertical: TextAlignVertical.center,
           textInputAction: TextInputAction.send,
           onSubmitted: (final String value) {
-            submit(langbarState, context);
+            submit(context);
           },
           decoration: InputDecoration(
             hintText: 'Type here what you want',
@@ -77,21 +69,10 @@ class _LangFieldState extends State<LangField> {
     return row;
   }
 
-  void submit(LangBarState langbarState, BuildContext context) {
-    var apiKey2 = getOpenAIKey();
-    var client =
-        OpenAIClient.instanceFor(apiKey: apiKey2, apiBaseUrl: getLlmBaseUrl());
-    var sessionToken = getSessionToken();
-    if (sessionToken != null) {
-      dart_openai.OpenAI.includeHeaders({"session": sessionToken});
-    }
-    final llm =
-        ChatOpenAI(apiClient: client, temperature: 0.0, model: 'gpt-3.5-turbo');
-    langbarState.sendingToOpenAI = true;
-    sendToOpenai(llm, this._controllerOutlined.text, context);
+  void submit(BuildContext context) {
+    submitToLLM(
+        context, this._controllerOutlined.text, this._controllerOutlined);
   }
-
-  final memory = ConversationBufferMemory(returnMessages: true);
 
   Future toggleRecording() {
     var langbarState = Provider.of<LangBarState>(context, listen: false);
@@ -103,78 +84,11 @@ class _LangFieldState extends State<LangField> {
       print("listening state $isListening, status $status");
       if (status == "done") {
         var langbarState = Provider.of<LangBarState>(context, listen: false);
-        submit(langbarState, context);
+        submit(context);
         langbarState.listeningForSpeech = false;
         print("sending ${_controllerOutlined.text}");
       }
     });
-  }
-
-  Future<void> sendToOpenai(
-      ChatOpenAI llm, String query, BuildContext context) async {
-    // final forecastTool = ForecastScreen.getTool(GoRouter.of(context));
-    // final creditCardTool = CreditCardScreen.getTool(GoRouter.of(context));
-    var langbarState = Provider.of<LangBarState>(context, listen: false);
-    langbarState.historyShowing = false;
-    var tools = parseRouters(GoRouter.of(context), routes);
-
-    DateTime now = DateTime.now();
-    String formattedDate = DateFormat('yyyy-MM-ddTHH:mm:ssZ').format(now);
-    final agent = OpenAIFunctionsAgent.fromLLMAndTools(
-        systemChatMessage: SystemChatMessagePromptTemplate(
-          prompt: PromptTemplate(
-            inputVariables: {},
-            template:
-                'You are a helpful AI assistant. The current date and time is ${formattedDate}.',
-          ),
-        ),
-        llm: llm,
-        tools: tools,
-        memory: memory);
-    final executor = AgentExecutor(agent: agent);
-    // final res = await executor.run('What is 40 raised to the 0.43 power?');
-    var response;
-    try {
-      response = await executor.run(query);
-    } catch (e) {
-      response = e.toString();
-    }
-    _controllerOutlined.clear();
-    langbarState.sendingToOpenAI = false;
-    // if response contains spaces, we assume it is not a path, but a response from the AI (when this becomes too much of a hack, we should start responding from tools with more complex objects with fields etc.
-    var chatHistory = Provider.of<ChatHistory>(context, listen: false);
-    if (response.contains(' ')) {
-      chatHistory.add(HistoryMessage(text: query, isHuman: true));
-      chatHistory.add(HistoryMessage(text: response, isHuman: false));
-      langbarState.historyShowing = true;
-    } else // add the original query, but the navigation-uri-repsonse as the hyperlink when you click on it
-      chatHistory
-          .add(HistoryMessage(text: query, isHuman: true, navUri: response));
-  }
-
-  parseRouters(GoRouter, List<RouteBase> routes, {parentPath}) {
-    var tools = <GenericScreenTool>[];
-    for (var route in routes) {
-      String? newPath = null;
-      if (route is GoRoute) {
-        // route.path is only the local path. If the route e.g. points to a details screen, we have to prepend the path of the parent:
-        newPath = (parentPath != null ? parentPath + "/" : "") + route.path;
-      }
-      if (route is LlmGoRoute) {
-        var tool = GenericScreenTool(
-            goRouter: goRouter,
-            name: route.name,
-            push: route.modal,
-            path: newPath!,
-            description: route.description,
-            parameters: route.parameters);
-        tools.add(tool);
-      }
-      if (route.routes.isNotEmpty) {
-        tools.addAll(parseRouters(goRouter, route.routes, parentPath: newPath));
-      }
-    }
-    return tools;
   }
 }
 
