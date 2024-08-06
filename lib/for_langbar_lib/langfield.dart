7abff74a3f8fc43b2e4dd01f67e3b42ddf7150e0
Hans van Dam <hans@damsoft.nl>
2024-08-06 12:45:02 +0200

    back to the original langchain based version

--- lib/for_langbar_lib/langfield.dart
+++ lib/for_langbar_lib/langfield.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:langbar/for_langbar_lib/function_calling_v3/send_to_llm_trelis_flutter.dart';
 import 'package:langbar/for_langbar_lib/platform_details.dart';
+import 'package:langbar/for_langbar_lib/send_to_llm.dart';
 import 'package:langbar/for_langbar_lib/speech.dart';
 import 'package:langbar/for_langbar_lib/utils.dart';
 import 'package:provider/provider.dart';
@@ -69,8 +69,8 @@ class _LangFieldState extends State<LangField> {
   }
 
   void submit(BuildContext context) {
-    // submitToLLM(context);
-    submitToLLMNoLangchain(context);
+    submitToLLM(context);
+    // submitToLLMNoLangchain(context);
   }
 }
 
